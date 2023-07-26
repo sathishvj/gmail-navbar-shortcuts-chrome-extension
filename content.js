6c0a6b42d37c31704ddf0f5e0139dbbdbd2396ca
sathishvj <sathishvj@yahoo.com>
2023-07-26 10:15:56 +0530

    committing latest content.js with commented lines removed.

--- content.js
+++ content.js
@@ -77,7 +77,6 @@ const links = new Map([
     },
   ],
   [
-    // "all",
     "multipleMail",
     {
       url: "https://mail.google.com/mail/u/0/#all",
@@ -96,14 +95,3 @@ const links = new Map([
     },
   ],
 ]);
-
-// const svgs = {
-//   compose: "svgs/compose.svg",
-//   inbox: "svgs/inbox.svg",
-//   starred: "svgs/starred.svg",
-//   sent: "svgs/sent.svg",
-//   trash: "svgs/trash.svg",
-//   multipleMail: "svgs/multipleMail.svg",
-//   badge: "svgs/badge.svg",
-//   cloudy: "svgs/cloudy.svg",
-// };
